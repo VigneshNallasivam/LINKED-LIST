73f54f66e2d0730c0fe2fa54559da59218a85362
VigneshNallasivam <vickyshanestark@gmail.com>
2022-12-08 15:58:04 +0530

    Added : MASTER - Printing the Welcome Message For Linked List Program

---
+++ src/com/oops/basics/Display.java
@@ -0,0 +1,10 @@
+package com.oops.basics;
+
+public class Display 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome To Linked List Data Structure Simulation");
+	}
+
+}
